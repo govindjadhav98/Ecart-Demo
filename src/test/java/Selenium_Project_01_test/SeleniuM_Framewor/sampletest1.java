30485d6e3c273254e7744f81fa62fec4cc4fb19f
Govind <govindjadhav29199@gmail.com>
2024-01-18 13:20:19 +0530

    changes

--- src/test/java/Selenium_Project_01_test/SeleniuM_Framewor/sampletest1.java
+++ src/test/java/Selenium_Project_01_test/SeleniuM_Framewor/sampletest1.java
@@ -1,11 +1,5 @@
 package Selenium_Project_01_test.SeleniuM_Framewor;
 
-import org.testng.annotations.Test;
-
 public class sampletest1 {
 
-	@Test
-	public void gittest1() {
-		System.out.println("My github first code");
-	}
 }
